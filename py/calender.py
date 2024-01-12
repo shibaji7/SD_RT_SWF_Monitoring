ea0a106da22fccedc0ccae2b72f049d874a9dcd5
shibaji <shibaji7@vt.edu>
2024-01-12 08:50:11 -0500

    Updated Flare List

--- py/calender.py
+++ py/calender.py
@@ -13,19 +13,12 @@ __maintainer__ = "Chakraborty, S."
 __email__ = "shibaji7@vt.edu"
 __status__ = "Research"
 
-
-import sys
-
-sys.path.extend(["py/", "py/geo/"])
 import datetime as dt
-
+import os
 import pandas as pd
 from goes import FlareInfo
 
-if __name__ == "__main__":
-    year = 2021
-    start_date = dt.datetime(year, 1, 1)
-    end_date = dt.datetime.now()
+def create_flare_list_file(file, start_date, end_date):
     date = start_date
     events = pd.DataFrame()
     while date <= end_date:
@@ -34,18 +27,11 @@ if __name__ == "__main__":
         if len(g.flare) > 0:
             events = pd.concat([events, g.flare])
         date = date + dt.timedelta(1)
-    events.to_csv("repo/events.csv", index=False, header=True)
     events["clx"] = events.fl_goescls.apply(lambda x: x[0])
     events["date"] = events.event_peaktime.apply(
         lambda x: x.to_pydatetime().strftime("%Y-%m-%d")
     )
     date, color = list(), list()
-    brc = """
-    {
-    'date': new Date('fx_date'),
-    'color': 'fx_color',
-    },"""
-    txt = "const EVENTS = ["
     for d in events.date.unique():
         if d not in date:
             e = events[events.date == d]
@@ -58,10 +44,56 @@ if __name__ == "__main__":
                 if "X" in e.clx.tolist():
                     col = "red"
                 color.append(col)
-                txt += brc.replace("fx_color", col).replace("fx_date", du)
+    color_codes = pd.DataFrame()
+    color_codes["color"], color_codes["date"] = color, date
+    events.to_csv(file, index=False, header=True)
+    color_codes.to_csv(file.replace(".csv", "_color.csv"), index=False, header=True)
+    return events, color_codes
+
+def create_flare_list_for_calender(year_start):
+    events, color_codes = pd.DataFrame(), pd.DataFrame()
+    start_date = dt.datetime(year_start, 1, 1)
+    end_date = dt.datetime.now()
+    years = [year_start + i for i in range(int((end_date-start_date).days/365)+1)]
+    flare_data_files = ["assets/data/FL.{year}.csv".format(year=y) for y in years]
+    for file, year in zip(flare_data_files, years):
+        print(file, year)
+        if year == end_date.year:
+            e, code = create_flare_list_file(
+                    file, dt.datetime(year, 1, 1), 
+                    dt.datetime(year, 12, 31)
+                )
+            events = pd.concat([events, e])
+            color_codes = pd.concat([color_codes, code])
+        else:
+            if not os.path.exists(file):
+                e, code = create_flare_list_file(
+                    file, dt.datetime(year, 1, 1), 
+                    dt.datetime(year, 12, 31)
+                )
+                events = pd.concat([events, e])
+                color_codes = pd.concat([color_codes, code])
+            else:
+                events = pd.concat([
+                    events, 
+                    pd.read_csv(file, parse_dates=["event_starttime","event_peaktime","event_endtime","date"])
+                ])
+                color_codes = pd.concat([
+                    color_codes, 
+                    pd.read_csv(file.replace(".csv","_color.csv"), parse_dates=["date"])
+                ])
+    events.to_csv("assets/data/FL.csv", index=False, header=True)
+    color_codes.to_csv("assets/data/FL.color.csv", index=False, header=True)
+    brc = """
+    {
+    'date': new Date('fx_date'),
+    'color': 'fx_color',
+    },"""
+    txt = "const EVENTS = ["
+    for i, e in color_codes.iterrows():
+        txt += brc.replace("fx_color", e.color).replace("fx_date", 
+            e.date.strftime("%m/%d/%Y"))
     txt += "\n]"
-    with open("repo/constant.js", "w") as f:
+    with open("assets/js/constant.js", "w") as f:
         f.write(txt)
-    _obj = pd.DataFrame()
-    _obj["date"], _obj["color"] = date, color
-    _obj.to_csv("repo/eventlist.csv", index=False, header=True)
+    return events, color_codes
--- simulate.py
+++ simulate.py
@@ -11,3 +11,26 @@ __maintainer__ = "Chakraborty, S."
 __email__ = "shibaji7@vt.edu"
 __status__ = "Research"
 
+import argparse
+import sys
+sys.path.extend(["py/", "py/geo/"])
+from calender import create_flare_list_for_calender
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-y", "--year", default=2021, type=int, help="Start year for flare list creation."
+    )
+    parser.add_argument(
+        "-m", "--method", default="FL", type=str, help="FL: Flare list; EA: Event analysis; DRAP: Model"
+    )
+    args = parser.parse_args()
+    for k in vars(args).keys():
+        print("     ", k, "->", str(vars(args)[k]))
+    if args.method == "EA":
+        run_event(args.index, args.cfg_file)
+    elif args.method == "FL":
+        create_flare_list_for_calender(args.year)
+    else:
+        print(f"Invalid method / not implemented {args.method}")
