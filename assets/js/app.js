9b6c25cb72921ad5c8ca740942bde36b2f90c744
shibaji <shibaji7@vt.edu>
2024-01-15 15:17:41 -0500

    Updated js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 var calendar = document.querySelector('.calendar')
 
 fetchEvent = (date) => {
-    let color = 'white'
+    var color = 'white'
     for(i = 0; i<EVENTS.length; i++){
         e = EVENTS[i]
         if(e['date'].getTime()===date.getTime()){
@@ -12,7 +12,7 @@ fetchEvent = (date) => {
     return color
 }
 
-const month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+var month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
 isLeapYear = (year) => {
     return (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) || (year % 100 === 0 && year % 400 ===0)
@@ -24,35 +24,35 @@ getFebDays = (year) => {
 
 generateCalendar = (month, year) => {
     console.log(year, month)
-    let calendar_days = calendar.querySelector('.calendar-days')
-    let calendar_header_year = calendar.querySelector('#year')
+    var calendar_days = calendar.querySelector('.calendar-days')
+    var calendar_header_year = calendar.querySelector('#year')
 
-    let days_of_month = [31, getFebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    var days_of_month = [31, getFebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
     calendar_days.innerHTML = ''
 
-    let currDate = new Date()
+    var currDate = new Date()
     if (month > 11 || month < 0) month = currDate.getMonth()
     if (!year) year = currDate.getFullYear()
 
-    let curr_month = `${month_names[month]}`
+    var curr_month = `${month_names[month]}`
     month_picker.innerHTML = curr_month
     calendar_header_year.innerHTML = year
     console.log(year, month)
     // get first day of month
     
-    let first_day = new Date(year, month, 1)
-    let tag = `<span></span>
+    var first_day = new Date(year, month, 1)
+    var tag = `<span></span>
     <span></span>
     <span></span>
     <span></span>`
-    for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
-        let day = document.createElement('div')
+    for (var i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
+        var day = document.createElement('div')
         if (i >= first_day.getDay()) {
             day.classList.add('calendar-day-hover')
             day.innerHTML = i - first_day.getDay() + 1
             day.innerHTML += tag
-            let date = new Date(first_day.getFullYear(), first_day.getMonth(), i - first_day.getDay()+1)
+            var date = new Date(first_day.getFullYear(), first_day.getMonth(), i - first_day.getDay()+1)
             color = fetchEvent(date)
             // if (i - first_day.getDay() === currDate.getDate() && year === currDate.getFullYear() && month === currDate.getMonth()) {
             //     day.classList.add('curr-date')
@@ -68,10 +68,10 @@ generateCalendar = (month, year) => {
             }
         }
         day.addEventListener('click', function(e){
-            let href = window.location.href.replace('calender.html', '')
-            let date = e.target.innerHTML.replace(tag, '')
-            let month = first_day.getMonth() + 1
-            let year = first_day.getFullYear()
+            var href = window.location.href.replace('calender.html', '')
+            var date = e.target.innerHTML.replace(tag, '')
+            var month = first_day.getMonth() + 1
+            var year = first_day.getFullYear()
             if (month<=9){
                 month = '0' + month.toString()
             }
@@ -83,7 +83,7 @@ generateCalendar = (month, year) => {
             sd_img = "../assets/data/figures/rads/" + file;
             drap_img = "../assets/data/figures/drap/" + file;
             // folder = 'events/' + year + '-' + month + '-' + date + '/'
-            // let summary_page = href + folder + 'summary.html'
+            // var summary_page = href + folder + 'summary.html'
             console.log(goes_img, sd_img);
             // location.assign(summary_page)
             document.getElementById('calendar_pane').style.display = "none";
@@ -106,10 +106,10 @@ generateCalendar = (month, year) => {
 click_back = () =>{
     location.reload();
 }
-let month_list = calendar.querySelector('.month-list')
+var month_list = calendar.querySelector('.month-list')
 
 month_names.forEach((e, index) => {
-    let month = document.createElement('div')
+    var month = document.createElement('div')
     month.innerHTML = `<div data-month="${index}">${e}</div>`
     month.querySelector('div').onclick = () => {
         month_list.classList.remove('show')
@@ -119,16 +119,16 @@ month_names.forEach((e, index) => {
     month_list.appendChild(month)
 })
 
-let month_picker = calendar.querySelector('#month-picker')
+var month_picker = calendar.querySelector('#month-picker')
 
 month_picker.onclick = () => {
     month_list.classList.add('show')
 }
 
-let currDate = new Date()
+var currDate = new Date()
 
-let curr_month = {value: currDate.getMonth()}
-let curr_year = {value: currDate.getFullYear()}
+var curr_month = {value: currDate.getMonth()}
+var curr_year = {value: currDate.getFullYear()}
 
 generateCalendar(curr_month.value, curr_year.value)
 
