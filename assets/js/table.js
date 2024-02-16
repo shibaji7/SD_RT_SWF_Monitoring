d3ff168b84dd80f618546de014a12ba9ace1ea3d
shibaji <shibaji7@vt.edu>
2024-02-16 11:47:21 -0500

    Flare List

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,2 +1,21 @@
 
-function searchTable() {}
+function searchTable() {
+    var input, filter, table, tr, td, i, j, txtValue;
+    input = document.getElementById("table_input");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("flare_table");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those that don't match the search query
+    for (i = 1; i < tr.length; i++) { // Start from 1 to skip the header row
+        tr[i].style.display = "none"; // Hide row by default
+        td = tr[i].getElementsByTagName("td");
+        for (j = 0; j < td.length; j++) {
+            txtValue = td[j].textContent || td[j].innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = ""; // Show row if match found
+                break; // Break out of inner loop once a match is found in the row
+            }
+        }
+    }
+}
