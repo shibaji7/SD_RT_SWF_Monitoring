6be47892c42e9b8c65e25dca0bb21ee98d267758
Shibaji Chakraborty <shibaji7@vt.edu>
2024-07-17 08:39:20 -0400

    Updated on 2024-07-18

--- assets/js/flarelist.js
+++ assets/js/flarelist.js
@@ -3791,4 +3791,144 @@ var EVENTS = [
     'date': new Date('06/11/2024'),
     'color': 'green',
     },
+    {
+    'date': new Date('06/12/2024'),
+    'color': 'yellow',
+    },
+    {
+    'date': new Date('06/13/2024'),
+    'color': 'yellow',
+    },
+    {
+    'date': new Date('06/14/2024'),
+    'color': 'yellow',
+    },
+    {
+    'date': new Date('06/15/2024'),
+    'color': 'yellow',
+    },
+    {
+    'date': new Date('06/16/2024'),
+    'color': 'green',
+    },
+    {
+    'date': new Date('06/17/2024'),
+    'color': 'yellow',
+    },
+    {
+    'date': new Date('06/18/2024'),
+    'color': 'yellow',
+    },
+    {
+    'date': new Date('06/19/2024'),
+    'color': 'yellow',
+    },
+    {
+    'date': new Date('06/20/2024'),
+    'color': 'yellow',
+    },
+    {
+    'date': new Date('06/21/2024'),
+    'color': 'green',
+    },
+    {
+    'date': new Date('06/22/2024'),
+    'color': 'yellow',
+    },
+    {
+    'date': new Date('06/23/2024'),
+    'color': 'yellow',
+    },
+    {
+    'date': new Date('06/24/2024'),
+    'color': 'yellow',
+    },
+    {
+    'date': new Date('06/25/2024'),
+    'color': 'yellow',
+    },
+    {
+    'date': new Date('06/26/2024'),
+    'color': 'green',
+    },
+    {
+    'date': new Date('06/27/2024'),
+    'color': 'green',
+    },
+    {
+    'date': new Date('06/28/2024'),
+    'color': 'green',
+    },
+    {
+    'date': new Date('06/29/2024'),
+    'color': 'green',
+    },
+    {
+    'date': new Date('06/30/2024'),
+    'color': 'green',
+    },
+    {
+    'date': new Date('07/01/2024'),
+    'color': 'yellow',
+    },
+    {
+    'date': new Date('07/02/2024'),
+    'color': 'green',
+    },
+    {
+    'date': new Date('07/03/2024'),
+    'color': 'yellow',
+    },
+    {
+    'date': new Date('07/04/2024'),
+    'color': 'yellow',
+    },
+    {
+    'date': new Date('07/05/2024'),
+    'color': 'green',
+    },
+    {
+    'date': new Date('07/06/2024'),
+    'color': 'yellow',
+    },
+    {
+    'date': new Date('07/07/2024'),
+    'color': 'yellow',
+    },
+    {
+    'date': new Date('07/08/2024'),
+    'color': 'yellow',
+    },
+    {
+    'date': new Date('07/09/2024'),
+    'color': 'green',
+    },
+    {
+    'date': new Date('07/10/2024'),
+    'color': 'yellow',
+    },
+    {
+    'date': new Date('07/11/2024'),
+    'color': 'yellow',
+    },
+    {
+    'date': new Date('07/12/2024'),
+    'color': 'green',
+    },
+    {
+    'date': new Date('07/13/2024'),
+    'color': 'yellow',
+    },
+    {
+    'date': new Date('07/14/2024'),
+    'color': 'red',
+    },
+    {
+    'date': new Date('07/15/2024'),
+    'color': 'yellow',
+    },
+    {
+    'date': new Date('07/16/2024'),
+    'color': 'red',
+    },
 ]
